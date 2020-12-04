fabd645badcb5461d03591a788a7536b292d0111
Alexandre de Moura Boni <alexandre-cld@outlook.com>
2020-12-04 20:39:40 -0300

    player-1

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,17 @@
-function chooseSquare() {
-    console.log(`Clicou no botão`)
+var player = null
+var selectedPlayer = document.getElementById(`selected-player`)
+
+changePlayer(`X`)
+
+function chooseSquare(id) {
+    var square = document.getElementById(id)
+
+    square.innerHTML = player
+    square.style.color = `#000`
+}
+
+function changePlayer(value) {
+    player = value
+    selectedPlayer.innerHTML = player
+
 }
